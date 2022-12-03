a93dfbebe3332da6e9a4ddcd6520b58276705369
6thmoon <59669640+6thmoon@users.noreply.github.com>
2022-12-03 08:00:34 -0800

    Allies reveal enemy health; implement threshold.

--- Plugin.cs
+++ Plugin.cs
@@ -1,21 +1,57 @@
 ﻿using BepInEx;
 using HarmonyLib;
+using RoR2;
 using RoR2.UI;
 using System.Reflection;
+using System.Security.Permissions;
+using UnityEngine;
 
 [assembly: AssemblyVersion(Local.HealthBar.Viewer.Plugin.versionNumber)]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+		// Allow private member access via publicized assemblies.
 
 namespace Local.HealthBar.Viewer
 {
 	[BepInPlugin("local.healthbar.viewer", "HealthBarViewer", versionNumber)]
 	public class Plugin : BaseUnityPlugin
 	{
-		public const string versionNumber = "0.0.1";
-		public void Awake() => Harmony.CreateAndPatchAll(typeof(Plugin));
+		public const string versionNumber = "0.0.2";
+		public void Awake()
+		{
+			Harmony.CreateAndPatchAll(typeof(Plugin));
+			GlobalEventManager.onClientDamageNotified += ShowAllyTarget;
+		}
 
 		[HarmonyPatch(typeof(CombatHealthBarViewer), nameof(CombatHealthBarViewer.Awake))]
 		[HarmonyPrefix]
 		private static void IncreaseDuration(CombatHealthBarViewer __instance)
-				=> __instance.healthBarDuration = float.PositiveInfinity;
+				=> __instance.healthBarDuration = 8;
+
+		[HarmonyPatch(typeof(CombatHealthBarViewer), nameof(CombatHealthBarViewer.Update))]
+		[HarmonyPrefix]
+		private static void RemainVisible(CombatHealthBarViewer __instance)
+		{
+			foreach ( HealthComponent target in __instance.trackedVictims )
+				if ( target.combinedHealthFraction <= 0.75f )
+				{
+					var healthBarInfo = __instance.GetHealthBarInfo(target);
+					healthBarInfo.endTime = Mathf.Max(healthBarInfo.endTime, Time.time + 2);
+				}
+		}
+
+		private static void ShowAllyTarget(DamageDealtMessage message)
+		{
+			if ( ! message.victim || message.isSilent ||
+					TeamIndex.Player != TeamComponent.GetObjectTeam(message.attacker)
+				) return;
+
+			HealthComponent target = message.victim.GetComponent<HealthComponent>();
+			if ( ! target || target.dontShowHealthbar ) return;
+			TeamIndex index = TeamComponent.GetObjectTeam(message.victim);
+
+			foreach ( CombatHealthBarViewer viewer in CombatHealthBarViewer.instancesList )
+				if ( message.attacker != viewer.viewerBodyObject && viewer.viewerBodyObject )
+					viewer.HandleDamage(target, index);
+		}
 	}
 }
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+#### `0.0.2`
+- Allies will now reveal enemy health upon dealing damage. Please note that this feature has not been tested for multiplayer compatibility.
+- Reduced duration to eight seconds. However, this change only applies to targets above 75% HP.
+
 #### `0.0.1`
 - Enemy health bars remain visible indefinitely.
 - Additional features/configuration in a future update?
