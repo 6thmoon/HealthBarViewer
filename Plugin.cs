e93e21d9d57be715ec76db63ba427ff218e7f87a
6thmoon <59669640+6thmoon@users.noreply.github.com>
2022-12-09 20:40:12 -0800

    Add configuration file. Limit visibility beyond a certain range.
    Update icon and documentation.

--- Plugin.cs
+++ Plugin.cs
@@ -1,8 +1,11 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 using RoR2;
 using RoR2.UI;
+using System;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Security.Permissions;
 using UnityEngine;
 
@@ -15,43 +18,129 @@ namespace Local.HealthBar.Viewer
 	[BepInPlugin("local.healthbar.viewer", "HealthBarViewer", versionNumber)]
 	public class Plugin : BaseUnityPlugin
 	{
-		public const string versionNumber = "0.0.2";
+		public const string versionNumber = "0.1.0";
+
+		private static float duration, threshold, range;
+		private const float interval = 2.0f;
+
 		public void Awake()
 		{
+			const string section = "General";
+
+			duration = Config.Bind(
+					section: section,
+					key: "Minimum Duration",
+					defaultValue: 8u,
+					description: "After an ally deals damage, the target's health bar will" +
+						" remain visible for this many seconds."
+				).Value;
+
+			threshold = Config.Bind(
+					section: section,
+					key: "Health Threshold",
+					defaultValue: 75f,
+					new ConfigDescription(
+						"Enemies at this hit point percentage (or below) remain visible" +
+							" indefinitely.",
+						new AcceptableValueRange<float>(0, 100))
+				).Value / 100;
+
+			range = Config.Bind(
+					section: section,
+					key: "Maximum Range",
+					defaultValue: 120u,
+					description:
+						"Remove health bar regardless of health threshold if distance to " +
+						"target exceeds this value in meters. Set to zero for unlimited range."
+				).Value;
+
+			ReplaceEventHandler();
 			Harmony.CreateAndPatchAll(typeof(Plugin));
-			GlobalEventManager.onClientDamageNotified += ShowAllyTarget;
+		}
+
+		private static void ReplaceEventHandler()
+		{
+			try
+			{
+				Type healthBarViewer = typeof(CombatHealthBarViewer),
+						eventManager = typeof(GlobalEventManager);
+				string eventName = nameof(GlobalEventManager.onClientDamageNotified);
+
+				RuntimeHelpers.RunClassConstructor(healthBarViewer.TypeHandle);
+
+				EventInfo eventInfo = eventManager.GetEvent(eventName);
+				Delegate eventHandler = eventManager.GetField(
+						eventName, BindingFlags.Static | BindingFlags.NonPublic
+					).GetValue(null) as Delegate;
+
+				foreach ( Delegate subscriber in eventHandler.GetInvocationList() )
+					if ( healthBarViewer == subscriber.Method.DeclaringType.DeclaringType )
+					{
+						eventInfo.RemoveEventHandler(null, subscriber);
+						return;
+					}
+
+				throw new Exception("Unable to locate original health bar event handler.");
+			}
+			catch ( Exception exception )
+			{
+				System.Console.WriteLine(exception);
+			}
+			finally
+			{
+				GlobalEventManager.onClientDamageNotified += ShowHealthBar;
+			}
 		}
 
 		[HarmonyPatch(typeof(CombatHealthBarViewer), nameof(CombatHealthBarViewer.Awake))]
 		[HarmonyPrefix]
 		private static void IncreaseDuration(CombatHealthBarViewer __instance)
-				=> __instance.healthBarDuration = 8;
+				=> __instance.healthBarDuration = duration;
 
-		[HarmonyPatch(typeof(CombatHealthBarViewer), nameof(CombatHealthBarViewer.Update))]
+		[HarmonyPatch(typeof(CombatHealthBarViewer), nameof(CombatHealthBarViewer.CleanUp))]
 		[HarmonyPrefix]
-		private static void RemainVisible(CombatHealthBarViewer __instance)
+		private static bool CheckStatus(CombatHealthBarViewer __instance)
 		{
-			foreach ( HealthComponent target in __instance.trackedVictims )
-				if ( target.combinedHealthFraction <= 0.75f )
+			for ( int i = __instance.trackedVictims.Count; --i >= 0; )
+			{
+				HealthComponent target = __instance.trackedVictims[i];
+				CombatHealthBarViewer.HealthBarInfo
+						healthBarInfo = __instance.GetHealthBarInfo(target);
+
+				if ( target && target.alive && target.body )
 				{
-					var healthBarInfo = __instance.GetHealthBarInfo(target);
-					healthBarInfo.endTime = Mathf.Max(healthBarInfo.endTime, Time.time + 2);
+					if ( healthBarInfo.endTime > Time.time ) continue;
+
+					float distance = range > 0 && __instance.viewerBody ? (
+							__instance.viewerBody.corePosition - target.body.corePosition
+						).magnitude : range;
+
+					if ( target.combinedHealthFraction <= threshold && distance <= range )
+					{
+						healthBarInfo.endTime = Time.time + interval;
+						continue;
+					}
 				}
+
+				__instance.Remove(i, healthBarInfo);
+			}
+
+			return false;
 		}
 
-		private static void ShowAllyTarget(DamageDealtMessage message)
+		private static void ShowHealthBar(DamageDealtMessage message)
 		{
-			if ( ! message.victim || message.isSilent ||
-					TeamIndex.Player != TeamComponent.GetObjectTeam(message.attacker)
-				) return;
+			if ( message.isSilent || ! message.victim || ! message.attacker ) return;
 
 			HealthComponent target = message.victim.GetComponent<HealthComponent>();
 			if ( ! target || target.dontShowHealthbar ) return;
-			TeamIndex index = TeamComponent.GetObjectTeam(message.victim);
+
+			TeamIndex attacker = TeamComponent.GetObjectTeam(message.attacker),
+					victim = TeamComponent.GetObjectTeam(message.victim);
 
 			foreach ( CombatHealthBarViewer viewer in CombatHealthBarViewer.instancesList )
-				if ( message.attacker != viewer.viewerBodyObject && viewer.viewerBodyObject )
-					viewer.HandleDamage(target, index);
+				if ( viewer.viewerBodyObject && attacker == viewer.viewerTeamIndex )
+					viewer.HandleDamage(target, victim);
 		}
 	}
 }
--- README.md
+++ README.md
@@ -1,7 +1,19 @@
+## Introduction
+
+In the current version of the game, enemy health bars disappear if you don't deal damage to them for more than three seconds. However, this plugin increases that duration, and allows enemies at low health and/or nearby the player to remain visible indefinitely. Allies will now also reveal their target upon dealing damage.
+
+All of these parameters are easily customizable via the built-in [*r2modman*](https://thunderstore.io/package/ebkr/r2modman/) configuration editor. Simply change the options provided in the file `local.healthbar.viewer.cfg` - please refer to each entry description for more details. Feel free to report any feedback or issues discovered [here](https://github.com/6thmoon/HealthBarViewer/issues).
+
+## Version History
+
+#### `0.1.0`
+- Add configuration file; remove placeholder icon.
+- Introduce parameter to limit visibility beyond a certain range.
+- Publish source code and update documentation.
+
 #### `0.0.2`
-- Allies will now reveal enemy health upon dealing damage. Please note that this feature has not been tested for multiplayer compatibility.
-- Reduced duration to eight seconds. However, this change only applies to targets above 75% HP.
+- Allies reveal enemy health upon dealing damage.
+- Reduced duration to eight seconds for targets above 75% HP.
 
 #### `0.0.1`
 - Enemy health bars remain visible indefinitely.
-- Additional features/configuration in a future update?
